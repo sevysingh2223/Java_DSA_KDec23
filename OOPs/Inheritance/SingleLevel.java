d8861e1b39229a99d09a46d697eb4c2f567077df
Sevy Singh <119286678+sevysingh2223@users.noreply.github.com>
2024-02-06 18:10:28 +0530

    Add files via upload

---
+++ OOPs/Inheritance/SingleLevel.java
@@ -0,0 +1,46 @@
+package OOPs.Inheritance;
+
+class Father{
+    Father(){
+        System.out.println("I am a father");
+    }
+    Father(String name){
+        System.out.println("father's name is  : " + name);
+    }
+    void greet(){
+        System.out.println("Welcome to Mittal's Family : Father");
+    }
+    void displayAge(){
+        System.out.println("Father age is 50");
+    }
+}
+class Son extends Father{
+    Son(){
+        super(); // calling the constructor of the parent class
+        System.out.println("I am a Son");
+    }
+    Son(String name){
+        
+        System.out.println("My Name is " +name);
+    }
+    void greet(){
+        super.greet();
+        System.out.println("Welcome to Mittal's Family : Son");
+    }
+    void displayAge(){
+        System.out.println("Son age is 20");
+    }
+}
+
+public class SingleLevel {
+    public static void main(String[] args) {
+        // Son P1 = new Son();
+        Son P2 = new Son("Kunal");
+        P2.displayAge();
+        P2.greet();
+    }
+}
+
+
+// Parent class : Teacher (result: input (5 subject marks) attendance() > 75%)
+// Child class : Student (result)
