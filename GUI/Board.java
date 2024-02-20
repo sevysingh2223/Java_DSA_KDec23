8c60a80b740aec39950bdbfb2fcd8ebc00504ecc
Sevy Singh <119286678+sevysingh2223@users.noreply.github.com>
2024-02-20 17:56:49 +0530

    Add files via upload

--- GUI/Board.java
+++ GUI/Board.java
@@ -5,25 +5,26 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
-public class Board extends JPanel{
+
+public class Board extends JPanel {
     BufferedImage bgImg;
-    public Board(){
+
+    public Board() {
         showBg();
     }
 
-    public void showBg(){
-        URL image = Board.class.getResource("ganbg.jpg");
-        try{
-            bgImg =ImageIO.read(image);
-        }
-        catch(IOException e){
+    public void showBg() {
+        URL image = Board.class.getResource("ganebg.jpg");
+        try {
+            bgImg = ImageIO.read(image);
+        } catch (IOException e) {
             System.out.println("Error reading the file ganebg.jpg");
             e.printStackTrace();
         }
     }
 
-    public void paintComponent(Graphics brush){
+    public void paintComponent(Graphics brush) {
         super.paintComponent(brush);
-        brush.drawImage(bgImg,0,0,1200,600,null);
+        brush.drawImage(bgImg, 0, 0, 1200, 600, null);
     }
 }
--- GUI/GUI.java
+++ GUI/GUI.java
@@ -2,15 +2,16 @@ import javax.swing.JFrame;
 
 public class GUI {
 
-    GUI(){
+    GUI() {
         // Create the frame.
         JFrame frame = new JFrame();
         frame.setSize(1200, 600);
         frame.setTitle("Game");
-        frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(new Board());
+        frame.setVisible(true);
+
     }
 
     public static void main(String[] args) {
